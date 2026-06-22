73964cd24a7a8da57b08c79e3d8a86251a0f797e
Kevin Tran <khanh@bimco.org>
2026-06-22 15:23:04 +0700

    fix: Calculate interpolational landmarks to satisfy required_coords number

--- src/face_detector/face_detector.cc
+++ src/face_detector/face_detector.cc
@@ -48,6 +48,24 @@ std::vector<float> FaceDetector::Detect(const uint8_t* data,
       landmarks.push_back(point.x * inv_w);
       landmarks.push_back(point.y * inv_h);
     }
+
+    if (landmarks.size() >= 206) {  // need indices up to 102 (102*2+1 = 205)
+      // 106: center of points 102 and 98 (x from 102/92, y from 102/98)
+      landmarks.push_back((landmarks[102 * 2] + landmarks[92 * 2]) / 2);
+      landmarks.push_back((landmarks[102 * 2 + 1] + landmarks[98 * 2 + 1]) / 2);
+      // 107: center of points 35 and 65
+      landmarks.push_back((landmarks[35 * 2] + landmarks[65 * 2]) / 2);
+      landmarks.push_back((landmarks[35 * 2 + 1] + landmarks[65 * 2 + 1]) / 2);
+      // 108: center of points 70 and 40
+      landmarks.push_back((landmarks[70 * 2] + landmarks[40 * 2]) / 2);
+      landmarks.push_back((landmarks[70 * 2 + 1] + landmarks[40 * 2 + 1]) / 2);
+      // 109: center of points 5 and 80 (left cheek center)
+      landmarks.push_back((landmarks[5 * 2] + landmarks[80 * 2]) / 2);
+      landmarks.push_back((landmarks[5 * 2 + 1] + landmarks[80 * 2 + 1]) / 2);
+      // 110: center of points 81 and 27 (right cheek center)
+      landmarks.push_back((landmarks[81 * 2] + landmarks[27 * 2]) / 2);
+      landmarks.push_back((landmarks[81 * 2 + 1] + landmarks[27 * 2 + 1]) / 2);
+    }
   }
 
   return landmarks;
